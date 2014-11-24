b0591d095fc213ecb8d9c93b1a3860d00b7d8d57
DiegoPino <dpino@krayon.cl>
2014-11-24 12:10:41 -0300

    some minor changes

--- theme/islandora-red-biodiversidad-dwca.tpl.php
+++ theme/islandora-red-biodiversidad-dwca.tpl.php
@@ -61,5 +61,40 @@
         <?php print $variables['metadata']; ?>
       <?php endif; ?>
  </div> 
+ 
+ 
+ <div class="panel-group" id="datastreams">
+   <div class="panel panel-default">
+ 	  <div class="panel-heading">
+ 	        <h4 class="panel-title">
+ 	          <a data-toggle="collapse" data-parent="#datastreams" href="#collapseFive">
+ 	            <i class="fa fa-download fa-x2"></i>
+ 				<?php print t('File details'); ?>
+ 	          </a>
+ 	        </h4>
+ 	      </div>
+  <div id="collapseFive" class="panel-collapse collapse">
+        <div class="panel-body">
+ <table class="table">
+   <tr>
+     <th><?php print t('ID'); ?></th>
+     <th><?php print t('Label'); ?></th>
+     <th><?php print t('Size'); ?></th>
+     <th><?php print t('Mimetype'); ?></th>
+     <th><?php print t('Created'); ?></th>
+   </tr>
+   <?php foreach($datastreams as $key => $value): ?>
+   <tr>
+       <td><?php if(isset($value['id'])): ?><?php print $value['id']; ?><?php endif; ?></td>
+       <td><?php if(isset($value['label_link'])): ?><?php print $value['label_link']; ?><?php endif; ?></td>
+       <td><?php if(isset($value['size'])): ?><?php print $value['size']; ?><?php endif; ?></td>
+       <td><?php if(isset($value['mimetype'])): ?><?php print $value['mimetype']; ?><?php endif; ?></td>
+       <td><?php if(isset($value['created_date'])): ?><?php print $value['created_date']; ?><?php endif; ?></td>
+   </tr>
+   <?php endforeach; ?>
+ </table>
+ </div>
+ 
+ 
 
 
