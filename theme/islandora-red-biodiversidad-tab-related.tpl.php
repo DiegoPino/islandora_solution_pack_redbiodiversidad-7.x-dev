db8f24cecb4d0d236542065a585db8546bc6877a
DiegoPino <dpino@krayon.cl>
2015-02-05 15:52:37 -0300

    Removed full list of relations.
    Too slow to display all at once. Only Semantic guys could be interested
    in all possible paths...

--- theme/islandora-red-biodiversidad-tab-related.tpl.php
+++ theme/islandora-red-biodiversidad-tab-related.tpl.php
@@ -15,8 +15,6 @@
 ?>
 <div class="islandora-related-objects islandora">
   <div class="islandora-related-content-wrapper clearfix">
-
-    <?php if($variables['related_sparql_results']): ?>
       <div>
   		<?php if(isset($variables['vlot'])): ?>
 		<h2><?php print t('Related Darwin Core Objects Graph'); ?></h2>
@@ -24,25 +22,8 @@
   			<?php print $variables['vlot'];?>
   		</div>
   		<?php endif; ?>
-        <h2><?php print t('Related Darwin Core Objects'); ?></h2>
-		<div>
-			<?php print $variables['related_sparql_results'];?>
-		</div>
-		
-        <ul>
-          <?php foreach ($related as $item): ?>
-            <li><?php print $item['relation']; ?>
-				<?php print "-> " .$item['model_label']; ?>
-				<?php if (!empty($item['object']->label))
-				{
-				print l($item['object']->label, "islandora/object/".$item['object']->id.""); 
-			}else{
-				print l($item['object']->id, "islandora/object/".$item['object']->id."");} ?>
-			</li>
-          <?php endforeach; ?>
-        </ul>
+    
       </div>
-    <?php endif; ?>
   </div>
 </div>
 
