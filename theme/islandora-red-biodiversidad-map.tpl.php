60b43a23e1ae26a9287a00bca9c1576a7a845bf4
DiegoPino <dpino@krayon.cl>
2015-06-14 20:39:03 -0400

    Theme mods

--- theme/islandora-red-biodiversidad-map.tpl.php
+++ theme/islandora-red-biodiversidad-map.tpl.php
@@ -5,3 +5,11 @@
  */
 ?>
 <?php print $results; ?>
+
+<?php if ($variables['wasgeocoded']):?>
+     
+      <p>
+      <?php print $variables['geodecoded'];?>
+      </p>
+<p>Nominatim Search Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png"></p>
+<?php endif; ?> 
